1e8d42a83cbabc97201662a082ff72750d67fa65
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-22 21:52:47 +0300

    Create configuration files

---
+++ src/main/java/com/vitalikasaty/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.vitalikasaty.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
